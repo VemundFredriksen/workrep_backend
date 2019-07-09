496ebc2da6f27503e65f5dd1969b695e8b5bd349
Vemund Fredriksen <vemund.fredriksen@hotmail.com>
2019-07-09 15:15:19 +0200

    Added swagger generation and some commenting

--- Workrep.Backend.API/Controllers/WorkplaceController.cs
+++ Workrep.Backend.API/Controllers/WorkplaceController.cs
@@ -20,6 +20,10 @@ namespace Workrep.Backend.API.Controllers
             this.DBContext = dbContext;
         }
 
+        /// <summary>
+        /// TODO : Should be removed
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public async Task<ActionResult<ClientWorkplace[]>> GetAllAsync()
         {
@@ -44,28 +48,49 @@ namespace Workrep.Backend.API.Controllers
                 }).ToArray();
         }
 
+        /// <summary>
+        /// Returns specified workplace
+        /// </summary>
+        /// <param name="organizationNumber">Organization number</param>
+        /// <returns>Workplace</returns>
         [HttpGet("{organizationNumber}")]
         public async Task<ActionResult<ClientWorkplace>> GetAsync(long organizationNumber)
         {
-            return DBContext.Workplace.Include(w => w.Review)
+            var workplace = DBContext.Workplace.Include(w => w.Review)
                 .Include(w => w.SuperOrganizationNumberNavigation)
                 .Include(w => w.WorkplaceBio)
-                .Select(w => new ClientWorkplace()
-                {
-                    EmployeeCount = w.Employees,
-                    OrganizationNumber = w.OrganizationNumber,
-                    Address = w.Address,
-                    Bio = w.WorkplaceBio == null ? null : w.WorkplaceBio.Bio,
-                    City = w.City,
-                    Country = w.Country,
-                    Homepage = w.Homepage,
-                    Name = w.Name,
-                    Rating = w.Review.Count == 0 ? 0.0F : (float)w.Review.Average(r => (decimal)r.Rating),
-                    ReviewCount = w.Review.Count,
-                    SuperName = w.SuperOrganizationNumberNavigation.Name,
-                    SuperOrganizationNumber = w.SuperOrganizationNumber,
-                    ZIP = w.Zip
-                }).SingleOrDefault(cw => cw.OrganizationNumber == organizationNumber);
+                .SingleOrDefault(w => w.OrganizationNumber == organizationNumber);
+
+            if (workplace == null)
+                return NotFound($"Workplace {organizationNumber} not found");
+
+            return new ClientWorkplace()
+            {
+                EmployeeCount = workplace.Employees,
+                OrganizationNumber = workplace.OrganizationNumber,
+                Address = workplace.Address,
+                Bio = workplace.WorkplaceBio == null ? null : workplace.WorkplaceBio.Bio,
+                City = workplace.City,
+                Country = workplace.Country,
+                Homepage = workplace.Homepage,
+                Name = workplace.Name,
+                Rating = workplace.Review.Count == 0 ? 0.0F : (float)workplace.Review.Average(r => (decimal)r.Rating),
+                ReviewCount = workplace.Review.Count,
+                SuperName = workplace.SuperOrganizationNumberNavigation.Name,
+                SuperOrganizationNumber = workplace.SuperOrganizationNumber,
+                ZIP = workplace.Zip
+            };
+        }
+
+        /// <summary>
+        /// Returns list of reviews associated with specified workplace
+        /// </summary>
+        /// <param name="organizationNumber">Organization number</param>
+        /// <returns>List of Reviews</returns>
+        [HttpGet("{organizationNumber}/reviews")]
+        public async Task<ActionResult<Review[]>> GetReviewsAsync(long organizationNumber)
+        {
+            return DBContext.Review.Where(r => r.WorkplaceOrganizationNumber == organizationNumber).ToArray();
         }
 
     }
--- Workrep.Backend.API/Startup.cs
+++ Workrep.Backend.API/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -28,7 +30,19 @@ namespace Workrep.Backend.API
         {
             services.AddMvc();
 
+            // Workrep Databasecontext
             services.AddDbContext<WorkrepContext>(options => options.UseSqlServer(Configuration.GetSection("Connectionstrings").GetValue<string>("Workrep")));
+
+            //Defining the Swagger genearator
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info() { Title = "Workrep Core API", Version = "1.0.0" });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +52,16 @@ namespace Workrep.Backend.API
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Workrep API V1");
+            });
 
             app.UseMvc();
         }
