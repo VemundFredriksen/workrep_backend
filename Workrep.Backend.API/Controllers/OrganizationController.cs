0fa8ce6c11000acdb391ed611a4fd3b48d9a49d8
Vemund Fredriksen <vemund.fredriksen@hotmail.com>
2019-07-09 12:59:58 +0200

    Added custom class for clientOrganization

--- Workrep.Backend.API/Controllers/OrganizationController.cs
+++ Workrep.Backend.API/Controllers/OrganizationController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Workrep.Backend.API.Models;
 using Workrep.Backend.DatabaseIntegration.Models;
 
 namespace Workrep.Backend.API.Controllers
@@ -18,28 +20,93 @@ namespace Workrep.Backend.API.Controllers
             DBContext = dbContext;
         }
 
+        /// <summary>
+        /// TODO : Should be removed
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
-        public async Task<ActionResult<Organization[]>> GetAllAsync()
+        public async Task<ActionResult<ClientOrganization[]>> GetAllAsync()
         {
-            return DBContext.Organization.ToArray();
+            return DBContext.Organization.Include(o => o.OrganizationBio)
+                .Select(o => new ClientOrganization()
+                {
+                    OrganizationNumber = o.OrganizationNumber,
+                    Name = o.Name,
+                    Homepage = o.Homepage,
+                    City = o.City,
+                    Country = o.Country,
+                    Address = o.Address,
+                    EmployeeCount = o.Employees,
+                    SectorCode = o.SectorCode,
+                    Type = o.Type,
+                    ZIP = o.Zip
+                })
+                .ToArray();
         }
 
+        /// <summary>
+        /// Returns the specified organization
+        /// </summary>
+        /// <param name="organizationNumber">Organization number</param>
+        /// <returns>Organization</returns>
         [HttpGet("{organizationNumber}")]
-        public async Task<ActionResult<Organization>> GetAsync(long organizationNumber)
+        public async Task<ActionResult<ClientOrganization>> GetAsync(long organizationNumber)
         {
-            var organization = DBContext.Organization.SingleOrDefault(org => org.OrganizationNumber == organizationNumber);
+            var organization = DBContext.Organization.Include(o => o.OrganizationBio)
+                .Include(o => o.Workplace)
+                .ThenInclude(w => w.Review)
+                .Select(o => new ClientOrganization()
+                {
+                    OrganizationNumber = o.OrganizationNumber,
+                    Name = o.Name,
+                    Homepage = o.Homepage,
+                    City = o.City,
+                    Country = o.Country,
+                    Address = o.Address,
+                    EmployeeCount = o.Employees,
+                    SectorCode = o.SectorCode,
+                    Type = o.Type,
+                    ZIP = o.Zip,
+                    Bio = o.OrganizationBio == null ? null : o.OrganizationBio.Bio,
+                    WorkplaceCount = o.Workplace.Count,
+                    ReviewCount = o.Workplace.Sum(w => w.Review.Count),
+                    Rating = (float)o.Workplace.Sum(w => w.Review.Count) == 0 ? 0.0F : ((float)o.Workplace.Sum(w => w.Review.Sum(r => r.Rating))) / ((float)o.Workplace.Sum(w => w.Review.Count))
+                }).SingleOrDefault(org => org.OrganizationNumber == organizationNumber);
+
             if (organization == null)
                 return NotFound($"Organization {organizationNumber} not found!");
 
             return organization;
         }
 
+        /// <summary>
+        /// Returns list of all workplaces associated with specified organization
+        /// </summary>
+        /// <param name="organizationNumber">Organization number</param>
+        /// <returns>List of Workplaces</returns>
         [HttpGet("{organizationNumber}/workplaces")]
-        public async Task<ActionResult<Workplace[]>> GetWorkplacesAsync(long organizationNumber)
+        public async Task<ActionResult<ClientWorkplace[]>> GetWorkplacesAsync(long organizationNumber)
         {
-            return DBContext.Workplace.Where(work => work.SuperOrganizationNumber == organizationNumber).ToArray();
-            
+            return DBContext.Workplace.Include(w => w.Review)
+                .Include(w => w.WorkplaceBio)
+                .Include(w => w.SuperOrganizationNumberNavigation)
+                .Where(w => w.SuperOrganizationNumber == organizationNumber)
+                .Select(w => new ClientWorkplace()
+                {
+                    EmployeeCount = w.Employees,
+                    OrganizationNumber = w.OrganizationNumber,
+                    Address = w.Address,
+                    Bio = w.WorkplaceBio == null ? null : w.WorkplaceBio.Bio,
+                    City = w.City,
+                    Country = w.Country,
+                    Homepage = w.Homepage,
+                    Name = w.Name,
+                    Rating = w.Review.Count == 0 ? 0.0F : (float) w.Review.Average(r => (decimal) r.Rating),
+                    ReviewCount = w.Review.Count,
+                    SuperName = w.SuperOrganizationNumberNavigation.Name,
+                    SuperOrganizationNumber = w.SuperOrganizationNumber,
+                    ZIP = w.Zip
+                }).ToArray();
         }
-
     }
 }
---
+++ Workrep.Backend.API/Models/ClientOrganization.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Workrep.Backend.API.Models
+{
+    public class ClientOrganization
+    {
+        public long OrganizationNumber { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Homepage { get; set; }
+        public string SectorCode { get; set; }
+        public string Address { get; set; }
+        public int? ZIP { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public int? EmployeeCount { get; set; }
+
+        public string Bio { get; set; }
+        public int WorkplaceCount { get; set; }
+        public int ReviewCount { get; set; }
+        public float Rating { get; set; }
+    }
+}
