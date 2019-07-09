41806fd7251da17f3d318ee2f6cd0d077feb8c5b
Vemund Fredriksen <vemund.fredriksen@hotmail.com>
2019-07-09 13:00:19 +0200

    Added custom clientWorkplace class

---
+++ Workrep.Backend.API/Controllers/WorkplaceController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Workrep.Backend.API.Models;
+using Workrep.Backend.DatabaseIntegration.Models;
+
+namespace Workrep.Backend.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class WorkplaceController : Controller
+    {
+
+        public WorkrepContext DBContext { get; set; }
+
+        public WorkplaceController(WorkrepContext dbContext)
+        {
+            this.DBContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ClientWorkplace[]>> GetAllAsync()
+        {
+            return DBContext.Workplace.Include(w => w.Review)
+                .Include(w => w.SuperOrganizationNumberNavigation)
+                .Include(w => w.WorkplaceBio)
+                .Select(w => new ClientWorkplace()
+                {
+                    EmployeeCount = w.Employees,
+                    OrganizationNumber = w.OrganizationNumber,
+                    Address = w.Address,
+                    Bio = w.WorkplaceBio == null ? null : w.WorkplaceBio.Bio,
+                    City = w.City,
+                    Country = w.Country,
+                    Homepage = w.Homepage,
+                    Name = w.Name,
+                    Rating = w.Review.Count == 0 ? 0.0F : (float)w.Review.Average(r => (decimal)r.Rating),
+                    ReviewCount = w.Review.Count,
+                    SuperName = w.SuperOrganizationNumberNavigation.Name,
+                    SuperOrganizationNumber = w.SuperOrganizationNumber,
+                    ZIP = w.Zip
+                }).ToArray();
+        }
+
+        [HttpGet("{organizationNumber}")]
+        public async Task<ActionResult<ClientWorkplace>> GetAsync(long organizationNumber)
+        {
+            return DBContext.Workplace.Include(w => w.Review)
+                .Include(w => w.SuperOrganizationNumberNavigation)
+                .Include(w => w.WorkplaceBio)
+                .Select(w => new ClientWorkplace()
+                {
+                    EmployeeCount = w.Employees,
+                    OrganizationNumber = w.OrganizationNumber,
+                    Address = w.Address,
+                    Bio = w.WorkplaceBio == null ? null : w.WorkplaceBio.Bio,
+                    City = w.City,
+                    Country = w.Country,
+                    Homepage = w.Homepage,
+                    Name = w.Name,
+                    Rating = w.Review.Count == 0 ? 0.0F : (float)w.Review.Average(r => (decimal)r.Rating),
+                    ReviewCount = w.Review.Count,
+                    SuperName = w.SuperOrganizationNumberNavigation.Name,
+                    SuperOrganizationNumber = w.SuperOrganizationNumber,
+                    ZIP = w.Zip
+                }).SingleOrDefault(cw => cw.OrganizationNumber == organizationNumber);
+        }
+
+    }
+}
---
+++ Workrep.Backend.API/Models/ClientWorkplace.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Workrep.Backend.API.Models
+{
+    public class ClientWorkplace
+    {
+
+        public long OrganizationNumber { get; set; }
+        public long? SuperOrganizationNumber { get; set; }
+        public string Name { get; set; }
+        public int? EmployeeCount { get; set; }
+        public string Homepage { get; set; }
+        public string Address { get; set; }
+        public int? ZIP { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+        public string SuperName { get; set; }
+        public int ReviewCount { get; set; }
+        public float Rating { get; set; }
+        public string Bio { get; set; }
+
+    }
+}
