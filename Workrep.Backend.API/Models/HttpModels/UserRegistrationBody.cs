7b3df4cf4e7356bc44955e3455a90e822b82be02
Vemund Fredriksen <vemund.fredriksen@hotmail.com>
2019-07-26 19:12:22 +0200

    Restructured folders

---
+++ Workrep.Backend.API/Models/HttpModels/UserRegistrationBody.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Workrep.Backend.API.Models.HttpModels
+{
+    public class UserRegistrationBody
+    {
+        public string Email { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public DateTime Birthdate { get; set; }
+    }
+}
